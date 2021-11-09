af84e50f4e5f8b6cca4c590888c0ac35155108ad
Herry Ildawan <herry.ildawan@outlook.com>
2021-11-09 16:22:27 +0700

    Add project files.

---
+++ XAFPOS.WebApi/Services/XpoDataStoreProviderAccessor.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using DevExpress.ExpressApp.Xpo;
+
+namespace XAFPOS.WebApi.Services {
+    public class XpoDataStoreProviderAccessor {
+        public IXpoDataStoreProvider DataStoreProvider { get; set; }
+    }
+}
